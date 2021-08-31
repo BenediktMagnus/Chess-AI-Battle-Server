ee8793360bb821603f3734ba01692196cf9efb26
Benedikt Magnus <magnus@magnuscraft.de>
2021-08-31 22:15:55 +0200

    Added some documentation.

--- src/backend/game/gameState.ts
+++ src/backend/game/gameState.ts
@@ -1,10 +1,17 @@
 export enum GameState
 {
+    /** The game is running. */
     Running = 'r',
+    /** The current player is in check. */
     Check = 'c',
+    /** The current player is checkmate, thus the other player has won. */
     Checkmate = 'm',
+    /** Draw because the current player is in stalemate. */
     Stalemate = 's',
+    /** Draw because of the fifty-move rule. */
     FiftyMoveRule = 'f',
+    /** Draw because of threefold repetition. */
     ThreefoldRepetition = 't',
+    /** Draw because of insufficient material. */
     InsufficientMaterial = 'i',
 }
--- src/backend/game/moveResult.ts
+++ src/backend/game/moveResult.ts
@@ -1,8 +1,13 @@
 export enum MoveResult
 {
+    /** The move has been successfull. */
     Success = 's',
+    /** It is not the player's turn. */
     NotYourTurn = 't',
+    /** The given move notation is invalid. */
     InvalidNotation = 'n',
+    /** The given promotion piece is invalid. */
     InvalidPromotion = 'p',
+    /** The given move is not legal. */
     IllegalMove = 'm',
 }
