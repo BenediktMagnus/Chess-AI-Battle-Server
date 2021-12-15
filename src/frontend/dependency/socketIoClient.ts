b3236a6de689b7265184792137f4fc9dc282eb88
Benedikt Magnus <magnus@magnuscraft.de>
2021-12-15 10:01:25 +0100

    Set socket.io client import to type only.

--- src/frontend/dependency/socketIoClient.ts
+++ src/frontend/dependency/socketIoClient.ts
@@ -1,4 +1,4 @@
-import { io as ioType } from 'socket.io-client';
+import type { io as ioType } from 'socket.io-client';
 // @ts-expect-error Workaround/Hack for imports of ESM modules from paths relative to the base URL in Typescript:
 import { io as rawIo } from '/socket.io/socket.io.esm.min.js';
 
