39443437d176efc2eb881d0d5b4a31a516dbb55d
Benedikt Magnus <magnus@magnuscraft.de>
2022-02-21 17:42:11 +0100

    Implemented all viewer events in the frontend.

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,6 +1,7 @@
 import * as TypedSocketIo from './dependency/typedSocketIo';
-import { Chessboard } from 'cm-chessboard';
+import { Chessboard, PIECE as Piece, Square } from 'cm-chessboard';
 import { io } from './dependency/socketIoClient';
+import { PlayerStatistic } from '../shared/playerStatistic';
 import { Translator } from './localisation/translator';
 import { Ui } from './ui/ui';
 import { Utils } from './utility/utils';
@@ -16,6 +17,11 @@ class Main
     {
         this.translator = new Translator();
         this.socket = io();
+
+        this.socket.on('initiate', Utils.catchVoidPromise(this.onInitiate.bind(this)));
+        this.socket.on('move', Utils.catchVoidPromise(this.onMove.bind(this)));
+        this.socket.on('startNextGame', Utils.catchVoidPromise(this.onStartNextGame.bind(this)));
+        this.socket.on('end', Utils.catchVoidPromise(this.onEnd.bind(this)));
 
         this.socket.connect();
 
@@ -56,6 +62,72 @@ class Main
                 },
             }
         );
+
+        this.socket.emit('register');
+    }
+
+    private async onInitiate (board: string, rounds: number, playerStatistics: PlayerStatistic[]): Promise<void>
+    {
+        if ((this.ui === null) || (this.chessboard === null))
+        {
+            throw new Error('Server sent an initiate event while the UI is not fully initialised.');
+        }
+
+        this.ui.setRounds(rounds);
+
+        if (playerStatistics.length > 0)
+        {
+            this.ui.setStatistics(playerStatistics);
+        }
+
+        await this.chessboard.setPosition(board);
+    }
+
+    private async onMove (move: string): Promise<void>
+    {
+        if (this.chessboard === null)
+        {
+            throw new Error('Server sent a move event while chessboard is not initialised.');
+        }
+
+        const from = move.substring(0, 2) as Square;
+        const to = move.substring(2, 4) as Square;
+
+        await this.chessboard.movePiece(from, to);
+
+        if (move.length > 4)
+        {
+            const promotion = move.substring(4, 5);
+
+            const oldPiece = this.chessboard.getPiece(to);
+
+            const newPiece = (oldPiece.charAt(0) + promotion) as Piece; // Convert old piece to new piece of the same colour.
+
+            this.chessboard.setPiece(to, newPiece);
+        }
+    }
+
+    private async onStartNextGame (playerStatistics: PlayerStatistic[]): Promise<void>
+    {
+        if ((this.ui === null) || (this.chessboard === null))
+        {
+            throw new Error('Server sent an start next game event while the UI is not fully initialised.');
+        }
+
+        this.ui.increaseRounds();
+        this.ui.setStatistics(playerStatistics);
+
+        await this.chessboard.setPosition('start');
+    }
+
+    private async onEnd (): Promise<void>
+    {
+        if (this.chessboard === null)
+        {
+            throw new Error('Server sent a end event while chessboard is not initialised.');
+        }
+
+        await this.chessboard.setPosition('empty');
     }
 }
 
