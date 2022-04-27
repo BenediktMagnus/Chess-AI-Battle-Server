3cf25ba9ef0d5aaf083037c1c48bd3435e6a94f0
Benedikt Magnus <magnus@magnuscraft.de>
2022-04-27 23:30:37 +0200

    ESBuild: Added entry points for admin and play pages.

--- esbuild.js
+++ esbuild.js
@@ -1,13 +1,13 @@
 const esbuild = require('esbuild');
 
 config = {
-    entryPoints: ['src/frontend/main.ts'],
+    entryPoints: ['src/frontend/main.ts', 'src/frontend/pages/adminPage.ts', 'src/frontend/pages/playPage.ts'],
     bundle: true,
     sourcemap: true,
     target: 'es2021',
     platform: 'browser',
     tsconfig: 'src/frontend/tsconfig.json',
-    outfile: 'bin/frontend/main.js',
+    outdir: 'bin/frontend/',
 };
 
 esbuild.build(config).catch(() => process.exit(1));
